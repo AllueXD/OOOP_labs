1eea2ffe05133da9abfb0988be608f551f9bd8e3
Yana Skyrda <yana.skirda101010@gmail.com>
2019-12-08 23:02:26 +0200

    main with two processing options implemented

--- Project_image_processing/main.cpp
+++ Project_image_processing/main.cpp
@@ -1,11 +1,27 @@
-#include "mainwindow.h"
-
-#include <QApplication>
+#include"ImageProcessing.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    int amount = 0;
+    std::string ans;
+    std::cout << "Enter amount of pic for scan" << std::endl;
+    std::cin >> amount;
+    std::cout << "Do you want find contours using cv? (y,n)" << std::endl;
+    std::cin >> ans;
+    int colorStep = 255/(amount-1); // for depth of contours
+    int currRedColor = 0;
+    if (ans == "y") {
+        for (int i = 0; i < amount; i++) {
+            find_contour_cv(std::to_string(i) + ".jpg", currRedColor);
+            currRedColor += colorStep;
+        }
+    } else {
+        std::string first = "0.jpg";
+        std::string last = std::to_string(amount-1) + ".jpg";
+        for (int i = 0; i < amount; i++) {
+            find_contour_own(std::to_string(i) + ".jpg", first, last, currRedColor);
+            currRedColor += colorStep;
+        }
+    }
+    return 0;
 }
